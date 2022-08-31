c67cb4177775c6ef69f1e79a0cb2a910253c9819
betalantz <betalantz@gmail.com>
2022-08-31 09:52:37 -0700

    render right eyeColor on BabyHog

--- 09-dq-props-state-bosshog/src/components/BabyHog.js
+++ 09-dq-props-state-bosshog/src/components/BabyHog.js
@@ -6,26 +6,42 @@ import GlowingBaby from "../assets/glowing-eyes.png";
 
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
+const eyeColorMapper = {
+  blue: BlueBaby,
+  sun: SunBaby,
+  glowing: GlowingBaby
+}
 
 function BabyHog(props) {
+  const {eyeColor, hobby, name} = props
+
   function handleChangeWeight(e) {
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
   }
 
+  // let pigletImage
+  // if (eyeColor === "blue") {
+  //   pigletImage = BlueBaby
+  // } else if (eyeColor === "sun") {
+  //   pigletImage = SunBaby
+  // } else {
+  //   pigletImage = GlowingBaby
+  // }
+
   return (
     <li className="hogbabies">
-      <h1>Name</h1>
+      <h1>{name}</h1>
       <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
+      <h3>Hobby: {hobby}</h3>
+      <h4>Eye Color: {eyeColor}</h4>
 
       <button name="+">Increase Weight</button>
       <button name="-">Decrease Weight</button>
 
       <div className="hb-wrap">
         <img
-          src={normalBaby}
+          src={eyeColorMapper[eyeColor]}
           style={{ height: "200px" }}
           alt="MasterBlasterJrJr"
         />
