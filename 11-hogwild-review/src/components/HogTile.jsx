044ff47ec729519a178e84361181ea24724109a6
betalantz <betalantz@gmail.com>
2022-09-06 10:22:01 -0700

    complete show Details

--- 11-hogwild-review/src/components/HogTile.jsx
+++ 11-hogwild-review/src/components/HogTile.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import HogDetail from './HogDetail'
 
 export default function HogTile({ hog }) {
+  const [showDetails, setShowDetails] = useState(false)
   const { name, image} = hog
+
 //   console.log('image: ', image);
   return (
     <div className="ui card eight wide column pigTile">
-        <img src={image} alt={name} />
-        <h3>{name}</h3>
+      <div className="image">
+        <img src={image} />
+      </div>
+      <div className="content">
+        <h3 className="header">{name}</h3>
+      </div>
+      {showDetails && <HogDetail hog={hog} />}
+      <button
+        className="ui button"
+        onClick={() => setShowDetails(!showDetails)}
+      >
+        {showDetails ? "Less Info" : "More Info"}
+      </button>
+      {/* <button className="ui button" onClick={() => setIsHidden(true)}>
+        Hide Me{" "}
+        <span role="img" aria-label="snout">
+          🐽
+        </span>
+      </button> */}
     </div>
   )
 }
