22b2147f0923fce732d6d4a80746c89451f557e1
betalantz <betalantz@gmail.com>
2022-09-08 15:26:46 -0700

    enable In Stock toggle

--- 14-mock-cc-review-plansty/src/components/PlantCard.js
+++ 14-mock-cc-review-plansty/src/components/PlantCard.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({plant}) {
+
+
+  const [isInStock, setIsInStock] = useState(true)
+
+
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isInStock ? (
+        <button onClick={() => setIsInStock(!isInStock)} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={() => setIsInStock(!isInStock)}>Out of Stock</button>
       )}
     </li>
   );
