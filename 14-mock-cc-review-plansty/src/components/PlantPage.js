9a8df344634ff358e5245deeb0c095354150f1b4
betalantz <betalantz@gmail.com>
2022-09-08 14:39:40 -0700

    renders PlantCards

--- 14-mock-cc-review-plansty/src/components/PlantPage.js
+++ 14-mock-cc-review-plansty/src/components/PlantPage.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plantsArr, setPlantsArr] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(plantArr => setPlantsArr(plantArr))
+  }, [])
+  
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plantsArr}/>
     </main>
   );
 }
