cdac71aa6b1024a6e15b0bc42203ff0bb288a192
betalantz <betalantz@gmail.com>
2022-09-26 11:00:01 -0700

    complete all deliverables

--- 19-review-theater-work/app/models/role.rb
+++ 19-review-theater-work/app/models/role.rb
@@ -2,4 +2,27 @@ class Role < ActiveRecord::Base
 
     has_many :auditions
 
+    def actors
+        self.auditions.pluck(:actor) # all AR methods
+        # self.auditions.map {|a| a.actor} # vanilla Ruby
+    end
+
+    def locations
+        self.auditions.pluck(:location) # see #actors above
+    end
+
+    def lead
+        lead = self.auditions.find_by(hired: true)
+        if lead
+            lead
+        else
+            'no actor has been hired for this role'
+        end
+    end
+
+    def understudy
+        hired = self.auditions.where(hired: true).second # uses all AR methods
+        hired ? hired : 'no actor has been hired for understudy for this role'
+    end
+
 end
