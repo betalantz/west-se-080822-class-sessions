0270eb8dcb7f7ff3aab91bdb3d1c414187d801f9
betalantz <betalantz@gmail.com>
2022-09-26 11:28:18 -0700

    add comments and diagram

--- 19-review-theater-work/app/models/audition.rb
+++ 19-review-theater-work/app/models/audition.rb
@@ -3,6 +3,8 @@ class Audition < ActiveRecord::Base
     belongs_to :role
 
     def call_back
+        # self.hired = true
+        # self.save
         self.update(hired: true)
     end
   
--- 19-review-theater-work/app/models/role.rb
+++ 19-review-theater-work/app/models/role.rb
@@ -5,13 +5,17 @@ class Role < ActiveRecord::Base
     def actors
         self.auditions.pluck(:actor) # all AR methods
         # self.auditions.map {|a| a.actor} # vanilla Ruby
+        # self.auditions.map(&:actor) # same as above with proc syntax
     end
 
     def locations
         self.auditions.pluck(:location) # see #actors above
+        # auditions.pluck(:location) # Ruby will supply implicit self
+
     end
 
     def lead
+        #  lead = self.auditions.find{|audition| audition.hired } # AR method + Ruby method
         lead = self.auditions.find_by(hired: true)
         if lead
             lead
@@ -21,6 +25,9 @@ class Role < ActiveRecord::Base
     end
 
     def understudy
+        # hireds = self.auditions.filter{|aud| aud.hired} # Ruby's .filter will return a new array of select elements
+        # hireds = Audition.where(hired: true, role: self) # AR's .where also returns and array and can match multiple conditions
+        # hired = hireds[1] # just grabbing the second element of the filtered array
         hired = self.auditions.where(hired: true).second # uses all AR methods
         hired ? hired : 'no actor has been hired for understudy for this role'
     end
