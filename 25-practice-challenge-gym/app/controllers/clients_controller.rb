500d15d5c34336c2b17f0191b82996cb301a7c93
betalantz <betalantz@gmail.com>
2022-10-18 12:58:34 -0700

    bonus deliverables

--- 25-practice-challenge-gym/app/controllers/clients_controller.rb
+++ 25-practice-challenge-gym/app/controllers/clients_controller.rb
@@ -1,9 +1,24 @@
 class ClientsController < ApplicationController
 
-    before_action :find_resource
+    before_action :find_resource, except: :index
+
+    def index
+        render json: Client.all
+    end
 
     def show
-        render json: @resource
+        render json: @resource, serializer: TotalMembershipsSerializer
+    end
+
+    def update
+        @resource.update!(client_params)
+        render json: @resource, status: :accepted
+    end
+
+    private
+
+    def client_params
+        params.permit(:name, :age)
     end
 
 end
--- 25-practice-challenge-gym/app/controllers/gyms_controller.rb
+++ 25-practice-challenge-gym/app/controllers/gyms_controller.rb
@@ -1,10 +1,19 @@
 class GymsController < ApplicationController
 
     # before_action :find_gym
-    before_action :find_resource
+    before_action :find_resource, except: :index
+
+    def index
+        render json: Gym.all
+    end
 
     def show
         render json: @resource
+    end
+
+    def update
+        @resource.update!(gym_params)
+        render json: @resource, status: :accepted
     end
 
     def destroy
@@ -13,6 +22,10 @@ class GymsController < ApplicationController
     end
 
     private
+
+    def gym_params
+        params.permit(:name, :address)
+    end
 
     # def find_gym
     #     @gym = Gym.find(params[:id])
