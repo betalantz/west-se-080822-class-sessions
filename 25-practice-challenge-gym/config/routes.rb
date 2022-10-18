72dc23db02999d2d0c09cabfd1af328cb6be196d
betalantz <betalantz@gmail.com>
2022-10-18 14:56:58 -0700

    edit routes.rb in 25

--- 25-practice-challenge-gym/config/routes.rb
+++ 25-practice-challenge-gym/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :memberships
-  resources :gyms
-  resources :clients
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :memberships, only: :create
+  resources :clients, except: [:create, :destroy]
+  resources :gyms, except: :create
 end
