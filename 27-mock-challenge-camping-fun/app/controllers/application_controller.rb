bcbe47f39034a62b24efdf84803aee053c3e92b0
betalantz <betalantz@gmail.com>
2022-10-19 15:24:54 -0700

    complete camper show route

--- 27-mock-challenge-camping-fun/app/controllers/application_controller.rb
+++ 27-mock-challenge-camping-fun/app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
-  include ActionController::Cookies
+  # include ActionController::Cookies
+
+  wrap_parameters format: []
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  private
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found"}, status: :not_found
+  end
+  
 
 end
--- 27-mock-challenge-camping-fun/app/controllers/campers_controller.rb
+++ 27-mock-challenge-camping-fun/app/controllers/campers_controller.rb
@@ -3,4 +3,8 @@ class CampersController < ApplicationController
     def index
         render json: Camper.all
     end
+
+    def show
+        render json: Camper.find(params[:id]), serializer: CamperWithActivitiesSerializer
+    end
 end
