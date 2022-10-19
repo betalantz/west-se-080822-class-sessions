d7bb2bd54df99eedc07cc808f5a1c9fa54db4179
betalantz <betalantz@gmail.com>
2022-10-19 09:15:12 -0700

    complete scientist show route

--- 26-mock-challenge-cosmic-travel/app/controllers/application_controller.rb
+++ 26-mock-challenge-cosmic-travel/app/controllers/application_controller.rb
@@ -3,11 +3,16 @@ class ApplicationController < ActionController::API
     wrap_parameters format: []
 
     rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     private
 
     def render_invalid_response(exception)
         render json: { errors: exception.record.errors.full_messages}, status: :unprocessable_entity
     end
+
+    def render_not_found_response(exception)
+        render json: { error: "#{exception.model} not found"}, status: :not_found
+    end 
 
 end
--- 26-mock-challenge-cosmic-travel/app/controllers/scientists_controller.rb
+++ 26-mock-challenge-cosmic-travel/app/controllers/scientists_controller.rb
@@ -1,6 +1,18 @@
 class ScientistsController < ApplicationController
 
+    before_action :find_scientist, only: :show
+
     def index
         render json: Scientist.all
+    end
+
+    def show
+        render json: @scientist, serializer: ScientistWithPlanetsSerializer
+    end
+
+    private
+
+    def find_scientist
+        @scientist = Scientist.find(params[:id])
     end
 end
