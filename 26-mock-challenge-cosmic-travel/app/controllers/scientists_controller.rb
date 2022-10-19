44304b5deecebb1c3b1874403bec9357924c9205
betalantz <betalantz@gmail.com>
2022-10-19 09:21:06 -0700

    completes scientist create route

--- 26-mock-challenge-cosmic-travel/app/controllers/scientists_controller.rb
+++ 26-mock-challenge-cosmic-travel/app/controllers/scientists_controller.rb
@@ -10,7 +10,16 @@ class ScientistsController < ApplicationController
         render json: @scientist, serializer: ScientistWithPlanetsSerializer
     end
 
+    def create
+        scientist = Scientist.create!(scientist_params)
+        render json: scientist, status: :created
+    end
+
     private
+
+    def scientist_params
+        params.permit(:name, :field_of_study, :avatar)
+    end
 
     def find_scientist
         @scientist = Scientist.find(params[:id])
