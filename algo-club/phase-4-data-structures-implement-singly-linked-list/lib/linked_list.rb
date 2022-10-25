770dd0bc65c7fdfac59e990a7f0ef610e9004397
betalantz <betalantz@gmail.com>
2022-10-25 14:49:42 -0700

    implement prepend

--- algo-club/phase-4-data-structures-implement-singly-linked-list/lib/linked_list.rb
+++ algo-club/phase-4-data-structures-implement-singly-linked-list/lib/linked_list.rb
@@ -2,4 +2,15 @@ require_relative './node'
 
 class LinkedList
 
+    attr_accessor :head
+
+    def initialize(head = nil)
+        @head = head
+    end
+
+    def prepend(node)
+        node.next_node = head
+        self.head = node
+    end
+
 end
